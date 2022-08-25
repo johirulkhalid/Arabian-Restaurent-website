65549265c62d0ad768dfa58d3938f092af846f22
Johirul Khalid <111776170+johirulkhalid@users.noreply.github.com>
2022-08-26 00:31:05 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu-btn');
+
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
